d851298e0b08feb380d6a78db347ee8a9c32c370
Brenda <brendaj0214@gmail.com>
2023-03-10 23:34:33 -0800

    test added

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,9 +1,10 @@
-"use strict";
+'use strict';
 
 const { app } = require('../src/app.js');
 const supertest = require('supertest');
 const request = supertest(app);
 const { sequelizeDatabase } = require('../index.js');
+const { response } = require('express');
 
 beforeAll(async () => {
   await sequelizeDatabase.sync();
@@ -13,21 +14,20 @@ afterAll(async () => {
   await sequelizeDatabase.drop();
 });
 
-describe("/signup route", () => {
-  it("should create a new user", async () => {
-    let response = await request.post("/signup").send({
-      username:"brenda",
-      password:"321"
+describe('/signup route', () => {
+  it('should create a new user', async () => {
+    let response = await request.post('/signup').send({
+      username: 'brenda',
+      password: '321',
     });
 
     expect(response.status).toBe(200);
-    expect(response.body.username).toEqual("brenda");
+    expect(response.body.username).toEqual('brenda');
   });
 
-  it("should allow sign in", async ()=>{
-    let response = await request.post('./signin');
+  it('should allow sign in', async () => {
+    let response = await request.post('./signin').auth('brenda','321');
 
     expect(response.status).toBe(200);
   });
-  it("should get all user data")
 });
